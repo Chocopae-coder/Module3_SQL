3853ae8b0e7bb6dc55d7ccb38fa26b51049666e5
Chocopae <doxuantruong2111@gmail.com>
2026-04-04 07:51:50 +0700

    them file

--- Create_procedure.sql
+++ Create_procedure.sql
@@ -8,4 +8,31 @@ BEGIN
 END //
 
 DELIMITER ;
+DELIMITER //
+
+CREATE PROCEDURE getCusById (IN cusNum INT(11))
+BEGIN
+
+  SELECT * FROM customers WHERE customerNumber = cusNum;
+
+END //
+
+DELIMITER ;
+call getCusByID(175);
+DELIMITER //
+
+CREATE PROCEDURE GetCustomersCountByCity(
+    IN  in_city VARCHAR(50),
+    OUT total INT
+)
+BEGIN
+    SELECT COUNT(customerNumber)
+    INTO total
+    FROM customers
+    WHERE city = in_city;
+END//
+
+DELIMITER ;
+call GetCustomersCountByCity('Lyon',@total);
+select @total;
 
